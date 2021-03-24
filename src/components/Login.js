ca7835045c39dd2b4d8b6ae6bb2ba3f4e489b1fb
Varela69 <luxo_v7@hotmail.com>
2021-03-24 20:06:53 -0300

    Orden del login a medias

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import { Button, Input } from "antd";
+import { Button, Input, Row, Col } from "antd";
 import { useState } from "react";
 import axios from "axios";
 
@@ -6,36 +6,54 @@ function Login() {
   const [usuario, setUsuario] = useState("");
   const [pass, setPass] = useState("");
 
+  function handleClick(e) {
+    e.preventDefault();
+    axios
+      .post("http://sistema_inventario.test/api/login", {
+        email: usuario,
+        password: pass,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  }
+
   return (
     <div>
-      <Input
-        onChange={(e) => {
-          setUsuario(e.target.value);
-        }}
-        placeholder="Usuario"
-      />
-      <Input.Password
-        onChange={(e) => {
-          setPass(e.target.value);
-        }}
-        placeholder="Contraseña"
-      />
-      <Button
-        onClick={() => {
-          axios
-            .post("http://sistema_inventario.test/api/login", {
-              email: usuario,
-              password: pass,
-            })
-            .then((response) => {
-              console.log(response);
-            });
-        }}
-        danger
-        type="primary"
-      >
-        Enviar
-      </Button>
+      <form onSubmit={handleClick}>
+        <Row>
+          <Col offset={10}>
+            <h1>Login</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={4} offset={10}>
+            <Input
+              onChange={(e) => {
+                setUsuario(e.target.value);
+              }}
+              placeholder="Usuario"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col span={4} offset={10}>
+            <Input.Password
+              onChange={(e) => {
+                setPass(e.target.value);
+              }}
+              placeholder="Contraseña"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col offset={10}>
+            <Button htmlType="submit" danger type="primary">
+              Enviar
+            </Button>
+          </Col>
+        </Row>
+      </form>
     </div>
   );
 }
